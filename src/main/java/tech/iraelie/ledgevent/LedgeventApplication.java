95d88eb366b8ae237ac2882f86bd46e5adccd63b
Irakoze Elie <niyubwayoiraelie5777@gmail.com>
2026-09-07 15:25:32 +0200

    Scafolded LedgEvent application for proving delivery guarantees over an unreliable transport at the connection layer

---
+++ src/main/java/tech/iraelie/ledgevent/LedgeventApplication.java
@@ -0,0 +1,13 @@
+package tech.iraelie.ledgevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LedgeventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LedgeventApplication.class, args);
+	}
+
+}
