d5ea216e5facaaa2a52146944f66b978bcbecdc0
Irakoze Elie <niyubwayoiraelie5777@gmail.com>
2026-09-09 18:41:42 +0200

    Fixed Cross Origin Resource Sharing errors caused by setAllowedOrigins in registerStompEndpoints, and added jwtHandshakeInterceptor.

--- src/main/java/tech/iraelie/ledgevent/config/WebSocketConfig.java
+++ src/main/java/tech/iraelie/ledgevent/config/WebSocketConfig.java
@@ -1,17 +1,28 @@
 package tech.iraelie.ledgevent.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import tech.iraelie.ledgevent.security.JwtHandshakeInterceptor;
+import tech.iraelie.ledgevent.security.UserHandshakeHandler;
 
 @Configuration
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+    private final JwtHandshakeInterceptor jwtHandshakeInterceptor;
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws-connect").withSockJS();
+        registry
+                .addEndpoint("/ws-connect")
+                .setAllowedOrigins("http://localhost:5500")
+                .addInterceptors(jwtHandshakeInterceptor)
+                .setHandshakeHandler(new UserHandshakeHandler())
+                .withSockJS();
     }
 
     @Override
