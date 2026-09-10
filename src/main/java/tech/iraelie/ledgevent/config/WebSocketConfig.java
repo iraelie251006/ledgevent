558ea5dddc4534418ea2bda45a695ab0dd0950a6
Irakoze Elie <niyubwayoiraelie5777@gmail.com>
2026-09-10 21:55:56 +0200

    added user destination prefix in configure message broker.

--- src/main/java/tech/iraelie/ledgevent/config/WebSocketConfig.java
+++ src/main/java/tech/iraelie/ledgevent/config/WebSocketConfig.java
@@ -29,5 +29,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/topic", "/queue");
+        registry.setUserDestinationPrefix("/user");
     }
 }
