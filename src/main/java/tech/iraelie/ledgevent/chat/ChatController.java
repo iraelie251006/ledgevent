0b0305cefc2a07b058a9b74c8754ae5616a126b4
Irakoze Elie <niyubwayoiraelie5777@gmail.com>
2026-09-10 21:51:33 +0200

    implemented handleReplay method to respond to /chat endpoint

--- src/main/java/tech/iraelie/ledgevent/chat/ChatController.java
+++ src/main/java/tech/iraelie/ledgevent/chat/ChatController.java
@@ -1,16 +1,36 @@
 package tech.iraelie.ledgevent.chat;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
+import java.util.List;
 
-@RestController
+@Controller
+@RequiredArgsConstructor
 public class ChatController {
+    private final SimpMessagingTemplate messagingTemplate;
+    private final MessageStreamService messageStreamService;
+
     @MessageMapping("/chat.send")
-    @SendTo("/topic/messages")
-    public ChatMessage handleMessage(ChatMessage incoming, Principal principal) {
-        return new ChatMessage(principal.getName(), incoming.content());
+//    @SendTo("/topic/messages")
+    public ChatEvent handleMessage(ChatMessage incoming, Principal principal) {
+        ChatMessage message = new ChatMessage(principal.getName(), incoming.content());
+
+        messagingTemplate.convertAndSend(
+                "/topic/messages",
+                message
+        );
+
+        return messageStreamService.publish(message);
+    }
+
+    @MessageMapping("/chat.replay")
+    public void handleReplay(ReplayRequest request, Principal principal) {
+        List<ChatEvent> events = messageStreamService.replaySince(request.lastId());
+        messagingTemplate.convertAndSendToUser(principal.getName(), "/queue/replay", events);
     }
 }
